893ec85cdb29ba25d11b934da6b7e50c5303f474
Clay McKee <cmckee120993@gmail.com>
2022-10-12 11:14:42 -0700

    fixing git issues

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -89,3 +89,19 @@ router.post('/login', async (req, res) => {
         res.status(500).json(err);
     };
 });
+
+router.post('/logout', (req, res) => {
+    if(req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+
+})
+
+module.exports = router;
