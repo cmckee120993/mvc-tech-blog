feb2963fd0478683f36322d07c58470f77871636
Clay McKee <cmckee120993@gmail.com>
2022-10-24 16:11:45 -0700

    restructuring and organizing of server file

--- server.js
+++ server.js
@@ -5,22 +5,22 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
-const sequelize = require('./config/connection');
-
-const  SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({helpers});
+const sequelize = require('./config/connection');
+const  SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const sess = {
     secret: 'peace and love',
     cookie: {
-        expires: 10 * 60 * 1000
+        maxAge: 300000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
     },
-    resave: true,
-    rolling: true,
+    resave: false,
     saveUnintialized: true,
     store: new SequelizeStore({
         db: sequelize
@@ -29,15 +29,18 @@ const sess = {
 
 app.use(session(sess));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+const hbs = exphbs.create({helpers});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}`));
-})
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}.`);
+    sequelize.sync({ force: false });
+});
