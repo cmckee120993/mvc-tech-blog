4d6c33a8fc2979dc9b578d03ed5f5043d024e683
Clay McKee <cmckee120993@gmail.com>
2022-10-31 09:27:55 -0700

    updated and cleaned up models

--- models/comment.js
+++ models/comment.js
@@ -5,41 +5,13 @@ class Comment extends Model {}
 
 Comment.init(
     {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            allowNull: false,
-            autoIncrement: true
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'user',
-                key: 'id'
-            }
-        },
-        post_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'post',
-                key: 'id'
-            }
-        },
-        comment_text: {
+        body: {
             type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
+            allowNull: false
         }
     },
         {
-            sequelize,
-            freezeTableName: true,
-            underscored: true,
-            modelName: 'comment'
+            sequelize
         }
 );
 
--- models/index.js
+++ models/index.js
@@ -2,28 +2,19 @@ const User = require('./user');
 const Post = require('./post');
 const Comment = require('./comment');
 
-User.hasMany(Post, {
-    foreignKey: 'user_id'
-});
-
 Post.belongsTo(User, {
-    foreignKey: 'user_id'
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
 });
 
 Comment.belongsTo(User, {
-    foreignKey: 'user_id'
-});
-
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
-});
-
-User.hasMany(Comment, {
-    foreignKey: 'user_id'
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
 });
 
 Post.hasMany(Comment, {
-    foreignKey: 'post_id'
+    foreignKey: 'postId',
+    onDelete: 'CASCADE'
 });
 
 module.exports = {User, Post, Comment};
